0c53772da5fb035aeaae45b37c45ce133728f299
paddyurlich <paddyurlich@gmail.com>
2017-04-03 20:22:53 +1200

    last commit :

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "p", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "p", password: "p", except: [:index, :show]
 
   def index
      @articles = Article.all
